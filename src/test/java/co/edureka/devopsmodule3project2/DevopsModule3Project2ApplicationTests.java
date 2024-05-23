29bad61fefdce528dd446701ba00558af3358f6a
AfrikyDev <afrikywaa@gmail.com>
2024-05-22 23:43:00 -0400

    Initial commit

---
+++ src/test/java/co/edureka/devopsmodule3project2/DevopsModule3Project2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edureka.devopsmodule3project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsModule3Project2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
